69df90a50f5bfb2ee6bc30614a8b78cdda24db9e
lyltn <lyltn.21it@vku.udn.vn>
2025-06-25 10:15:10 +0700

    myclass

--- src/main/java/org/example/ktigerstudybe/service/documentList/DocumentListService.java
+++ src/main/java/org/example/ktigerstudybe/service/documentList/DocumentListService.java
@@ -60,6 +60,8 @@ public interface DocumentListService {
 
     void toggleVisibility(Long id);
 
+
+
     /**
      * Nhóm các bộ flashcard theo type, mỗi nhóm tối đa 'limit' phần tử
      */
@@ -70,4 +72,5 @@ public interface DocumentListService {
     Page<DocumentListResponse> listByUser(Long userId, Pageable pg);
     Page<DocumentListResponse> searchPublic(String keyword, Pageable pageable);
     Page<DocumentListResponse> searchByUser(Long userId, String keyword, Pageable pg);
+    Page<DocumentListResponse> searchPublicByTitleOrType(String keyword, Pageable pageable);
 }
